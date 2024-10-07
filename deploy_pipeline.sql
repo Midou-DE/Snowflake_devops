10b553c02b4b0567c6cff95324fd1d3aea0b18d6
Midou-DE <snowflake.qs2@gmail.com>
2024-10-07 16:41:46 +0200

    Update deploy_pipeline.sql

--- deploy_pipeline.sql
+++ deploy_pipeline.sql
@@ -1,2 +1,4 @@
 
 execute immediate from 'Eloquant/deploy_eloquant.sql' using (environment => '{{environment}}');
+
+execute immediate from 'Eloquant/deploy_colugel.sql' using (environment => '{{environment}}');
