5f87ed9cec9e43a126a541fc4c05aa862f2e1ca9
Midou-DE <snowflake.qs2@gmail.com>
2024-10-07 10:57:23 +0200

    Update deploy_eloquant.sql

--- Eloquant/deploy_eloquant.sql
+++ Eloquant/deploy_eloquant.sql
@@ -1,2 +1,3 @@
+USE ROLE ACCOUNTADMIN;
 execute immediate from 'deploy/create_table_3035.sql' using (environment => '{{environment}}');
 execute immediate from 'deploy/create_table_popin.sql' using (environment => '{{environment}}');
