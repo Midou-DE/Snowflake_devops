3a980faab72ceb5aef59c0ea68bd86701aa46763
Midou-DE <snowflake.qs2@gmail.com>
2024-10-07 16:43:20 +0200

    Update verify_pipeline.sql

--- verify_pipeline.sql
+++ verify_pipeline.sql
@@ -1,2 +1,4 @@
 
 execute immediate from 'Eloquant/verify_eloquant.sql' using (environment => '{{environment}}');
+
+execute immediate from 'Colugel/verify_colugel.sql' using (environment => '{{environment}}');
