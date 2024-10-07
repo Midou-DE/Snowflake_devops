08c9699bfb2922d325da5d89e9ae18399f7ec52a
Midou-DE <snowflake.qs2@gmail.com>
2024-10-07 16:24:38 +0200

    Update verify_eloquant.sql

--- Eloquant/verify_eloquant.sql
+++ Eloquant/verify_eloquant.sql
@@ -1,2 +1,4 @@
 
 execute immediate from 'verfiy/create_table_3035.sql' using (environment => '{{environment}}');
+execute immediate from 'verfiy/create_table_popin.sql' using (environment => '{{environment}}');
+execute immediate from 'verfiy/create_table_devis.sql' using (environment => '{{environment}}');
