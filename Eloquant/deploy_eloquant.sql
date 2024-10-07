cf8fbf667e73a0ad8d5b353091c688a100431fd5
Midou-DE <snowflake.qs2@gmail.com>
2024-10-07 10:43:34 +0200

    Update deploy_eloquant.sql

--- Eloquant/deploy_eloquant.sql
+++ Eloquant/deploy_eloquant.sql
@@ -1,1 +1,2 @@
 execute immediate from 'deploy/create_table_3035.sql' using (environment => '{{environment}}');
+execute immediate from 'deploy/create_table_popin.sql' using (environment => '{{environment}}');
