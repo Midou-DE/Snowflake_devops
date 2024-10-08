b00dab1774c583b1d5186545556cc3fd2472e159
Midou DE <ahmed.dekkar@Air-de-Ahmed.lan>
2024-10-08 19:53:40 +0200

    khrfjkbhjbg

--- verify_pipeline.sql
+++ verify_pipeline.sql
@@ -2,5 +2,3 @@
 execute immediate from 'Eloquant/verify_eloquant.sql' using (environment => '{{environment}}');
 
 execute immediate from 'Colugel/verify_colugel.sql' using (environment => '{{environment}}');
-
-execute immediate from 'Toolkit/verify_security.sql' using (environment => '{{environment}}');
