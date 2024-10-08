b4b4a50e5efdfefa2c2c9fc8ac74df1ba52ef26d
Midou DE <ahmed.dekkar@Air-de-Ahmed.lan>
2024-10-09 00:52:21 +0200

    correct the task

--- Snowflake_DML/deploy/create_task_udf_eloquant.sql
+++ Snowflake_DML/deploy/create_task_udf_eloquant.sql
@@ -6,7 +6,7 @@ CREATE OR REPLACE TASK add_numbers_task
   SCHEDULE = 'USING CRON 0 * * * * UTC'  -- Run the task every hour
   AS
   INSERT INTO udf_results (num1, num2, result)
-  SELECT RANDOM(1, 100), RANDOM(1, 100),add_two_numbers(RANDOM(1, 100), RANDOM(1, 100));
+  SELECT uniform(1, 10, random())  as num1, uniform(1, 10, random()) as num2 ,add_two_numbers(num1,num2);
 
 
 ALTER TASK add_numbers_task RESUME;
