88cae30497a883caedc4cbbdc3e5e6e1005fc3cb
Midou DE <ahmed.dekkar@Air-de-Ahmed.lan>
2024-10-08 19:32:56 +0200

    create a procedure

---
+++ Toolkit/deploy/create_procedure_grants_priv.sql
@@ -0,0 +1,34 @@
+USE DATABASE TEST_ADK;
+
+USE SCHEMA TOOLKIT;
+
+
+CREATE OR REPLACE PROCEDURE grants_privilege_to_role(
+    json_input_list ARRAY
+)
+RETURNS STRING
+LANGUAGE PYTHON
+RUNTIME_VERSION = '3.8'
+PACKAGES = ('snowflake-snowpark-python')
+HANDLER = 'grants_privilege_to_role'
+AS
+$$
+def process_json_function(json_input_list):
+    import json # Convert Snowflake VARIANT to Python dictionary
+    import snowflake.snowpark as snowpark
+
+   
+    #create a snowpark session
+    session = snowpark.Session.builder.getOrCreate()
+   
+    # Loop through roles and privileges
+    result = []
+    for item in json_input_list:
+        for privilege in item['privileges']:
+            grant_privelege = f"GRANT {privilege} ON {item['object_type']} {item['object_name']} TO ROLE {item['role']};"
+            session.sql(grant_privelege).collect()
+            result.append(grant_privelege)
+
+    return  result
+   
+$$;
--- Toolkit/deploy_security.sql
+++ Toolkit/deploy_security.sql
@@ -1,1 +1,3 @@
 execute immediate from 'deploy/create_schema.sql' using (environment => '{{environment}}');
+
+execute immediate from 'deploy/create_procedure_grants_priv.sql' using (environment => '{{environment}}');
