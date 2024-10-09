8daeb56cd6cd0068fdea65fc23a3b0451fc0a301
Midou DE <ahmed.dekkar@Air-de-Ahmed.lan>
2024-10-09 13:34:20 +0200

    edit the procedure create policy

--- Security/deploy/create_masking_policy_eloquant.sql
+++ Security/deploy/create_masking_policy_eloquant.sql
@@ -3,13 +3,13 @@ USE DATABASE TEST_ADK;
 USE SCHEMA TOOLKIT;
 
 CALL CREATE_MASKING_POLICYdev(
-    'ELOQUANT_FIRST_NAME',
-    'STRING',
-    'MONITORING_LMG.FIRST_NAME',
-    'High',
-    'ACCOUNTADMIN',
-    '***',
-    'ELOQUANT'
+    'ELOQUANT_FIRST_NAME', -- THE POLICY NAME
+    'STRING', -- TYPE OF THE COLUMN 
+    'MONITORING_LMG.FIRST_NAME', --  THE TAG NAME 
+    'High',  -- la valeur de tag
+    'ACCOUNTADMIN', -- the role  ivew everything
+    '***', --  the masking value 
+    'ELOQUANT' --  the chema where themasking policy gonna be created
 );
 
 ALTER TABLE ELOQUANT.ELOQUANT_3035
