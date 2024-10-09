9c4ff5725b9d5b59d375944996a650289aa80dc3
Midou DE <ahmed.dekkar@Air-de-Ahmed.lan>
2024-10-09 11:28:37 +0200

    deploy changes

--- Snowflake_Monitoring/deploy_monitoring.sql
+++ Snowflake_Monitoring/deploy_monitoring.sql
@@ -4,3 +4,5 @@ execute immediate from 'deploy/create_schema.sql' using (environment => '{{envir
 execute immediate from 'deploy/create_table_alerte.sql' using (environment => '{{environment}}');
 
 execute immediate from 'deploy/create_table_wh_monitoring.sql' using (environment => '{{environment}}');
+
+execute immediate from 'deploy/create_tags_first_last_name.sql' using (environment => '{{environment}}');
--- Toolkit/deploy_toolkit.sql
+++ Toolkit/deploy_toolkit.sql
@@ -1,3 +1,5 @@
 execute immediate from 'deploy/create_schema.sql' using (environment => '{{environment}}');
 
 execute immediate from 'deploy/create_procedure_grants_priv.sql' using (environment => '{{environment}}');
+
+execute immediate from 'deploy/create_procedure_masking_policy.sql' using (environment => '{{environment}}');
