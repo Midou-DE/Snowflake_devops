2a78f2c9df1b1be7057a213c702d512cc0ab65b5
Midou DE <ahmed.dekkar@Air-de-Ahmed.lan>
2024-10-09 12:06:36 +0200

    create masking policy and associted to a table

--- Security/deploy_security.sql
+++ Security/deploy_security.sql
@@ -1,1 +1,3 @@
 execute immediate from 'deploy/grant_role_priv_for_eloquant.sql' using (environment => '{{environment}}');
+
+execute immediate from 'deploy/create_masking_policy_eloquant.sql' using (environment => '{{environment}}');
