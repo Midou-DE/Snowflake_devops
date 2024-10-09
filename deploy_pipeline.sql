07ecc66ff55c972b785be6b601dc7f32856959a7
Midou DE <ahmed.dekkar@Air-de-Ahmed.lan>
2024-10-09 14:15:43 +0200

    orginize the files execution

--- deploy_pipeline.sql
+++ deploy_pipeline.sql
@@ -1,14 +1,13 @@
 
+execute immediate from 'Snowflake_Monitoring/deploy_monitoring.sql' using (environment => '{{environment}}');
+
 execute immediate from 'Eloquant/deploy_eloquant.sql' using (environment => '{{environment}}');
 
 execute immediate from 'Colugel/deploy_colugel.sql' using (environment => '{{environment}}');
 
 execute immediate from 'Toolkit/deploy_toolkit.sql' using (environment => '{{environment}}');
 
-execute immediate from 'Snowflake_Monitoring/deploy_monitoring.sql' using (environment => '{{environment}}');
 
 execute immediate from 'Security/deploy_security.sql' using (environment => '{{environment}}');
 
 execute immediate from 'Snowflake_DML/deploy_dml.sql' using (environment => '{{environment}}');
-
-execute immediate from 'Snowflake_Monitoring/deploy_monitoring.sql' using (environment => '{{environment}}');
