3211dc9131b7b820f9b699205def050711312011
Midou DE <ahmed.dekkar@Air-de-Ahmed.lan>
2024-10-09 17:31:41 +0200

    grant privilge to database

--- Security/deploy/grant_role_priv_for_eloquant.sql
+++ Security/deploy/grant_role_priv_for_eloquant.sql
@@ -8,3 +8,10 @@ CALL GRANTS_PRIVILEGE_TO_ROLE(
     {'role':'DATA_VIS','object_type': 'schema','object_name':'TEST_ADK.ELOQUANT', 'privileges': ['USAGE','CREATE TASK', 'CREATE TAG']}
 ]
 );
+
+CALL GRANTS_PRIVILEGE_TO_ROLE(
+[
+    {'role':'DATA_ENGINEER','object_type': 'DATABASE', 'object_name':'TEST_ADK', 'privileges': ['USAGE']},
+    {'role':'DATA_VIS','object_type': 'DATABASE','object_name':'TEST_ADK', 'privileges': ['USAGE']}
+]
+);
