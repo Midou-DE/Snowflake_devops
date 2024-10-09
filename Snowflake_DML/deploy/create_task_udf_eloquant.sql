5b0b7678b0b411560b68475370b7e130c580bf9b
Midou DE <ahmed.dekkar@Air-de-Ahmed.lan>
2024-10-09 15:38:56 +0200

    correct task eloquant

--- Snowflake_DML/deploy/create_task_udf_eloquant.sql
+++ Snowflake_DML/deploy/create_task_udf_eloquant.sql
@@ -1,7 +1,7 @@
 USE DATABASE TEST_ADK;
 USE SCHEMA ELOQUANT;
 
-CREATE OR ALTER TASK add_numbers_task
+CREATE OR REPLACE TASK add_numbers_task
   WAREHOUSE = QUICKSTART_WH  -- Specify the warehouse to run the task
   SCHEDULE = 'USING CRON 0 * * * * UTC'  -- Run the task every hour
   AS
@@ -9,4 +9,4 @@ CREATE OR ALTER TASK add_numbers_task
   SELECT uniform(1, 10, random())  as num1, uniform(1, 10, random()) as num2 ,add_two_numbers(num1,num2);
 
 
---CREATE OR ALTER TASK add_numbers_task RESUME;
+ALTER TASK add_numbers_task RESUME;
