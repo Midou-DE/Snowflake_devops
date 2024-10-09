d9299b9162c413702f5491d1e74e8b72cfe97e2a
Midou DE <ahmed.dekkar@Air-de-Ahmed.lan>
2024-10-09 11:21:44 +0200

    create new proc masking pol and tags

---
+++ Snowflake_Monitoring/deploy/create_tags_first_last_name.sql
@@ -0,0 +1,9 @@
+USE DATABASE TEST_ADK;
+
+USE SCHEMA MONITORING_LMG;
+
+CREATE OR REPLACE TAG first_name
+    COMMENT = 'This tags the column first_name';
+
+CREAcTE OR REPLACE TAG last_name
+    COMMENT = 'This tags the column first_name';
---
+++ Toolkit/deploy/create_procedure_masking_policy.sql
@@ -0,0 +1,28 @@
+USE DATABASE TEST_ADK;
+
+USE SCHEMA TOOLKIT;
+
+CREATE OR REPLACE PROCEDURE CREATE_MASKING_POLICYdev(
+    policy_name STRING,
+    column_data_type STRING,
+    tag_name STRING,
+    tag_value STRING,
+    excluded_role STRING,
+    masked_value STRING
+)
+RETURNS STRING
+LANGUAGE SQL
+EXECUTE AS CALLER
+AS
+$$
+BEGIN
+    EXECUTE IMMEDIATE
+    'CREATE OR REPLACE MASKING POLICY ' || policy_name || ' AS (VAL ' || column_data_type || ') RETURNS ' || column_data_type || ' ->
+    CASE
+        WHEN SYSTEM$GET_TAG_ON_CURRENT_COLUMN(''' || tag_name || ''') = ''' || tag_value || ''' 
+        AND CURRENT_ROLE() NOT IN (''' || excluded_role || ''') THEN ''' || masked_value || '''
+        ELSE VAL
+    END';
+    RETURN 'Masking policy ' || policy_name || ' created successfully.';
+END;
+$$;
